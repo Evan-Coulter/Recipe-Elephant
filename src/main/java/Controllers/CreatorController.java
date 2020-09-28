fbfa5d8e3ca7118bf2d23942ce1bc74a879fd5ff
Evan-Coulter <ercoulte@sfu.ca>
2020-09-28 15:39:04 -0700

    Add Recipe Functionality & Clean Imports,
    added more basic methods to recipe class and cleaned up unused imports in home and creator controllers.

--- src/main/java/Controllers/CreatorController.java
+++ src/main/java/Controllers/CreatorController.java
@@ -1,7 +1,7 @@
 package Controllers;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import Model.Recipe;
+import com.google.gson.Gson;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,12 +10,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicLong;
 
-public class CreatorController {
+public class CreatorController implements Initializable {
+    private Recipe recipe;
 
     @FXML
     private VBox stepVBox;
@@ -26,11 +28,11 @@ public class CreatorController {
     @FXML
     private TextArea stepTextArea;
 
-    public void addStep() throws IOException {
+    public void addStep() {
         addItem(stepVBox, stepTextArea);
     }
 
-    public void addIngredient() throws IOException {
+    public void addIngredient(){
         addItem(ingredientVBox, ingredientTextArea);
     }
 
@@ -39,23 +41,28 @@ public class CreatorController {
      * whatever text was in the text area. USED TO UPDATE TWO INNER VBOXS IN CREATE RECIPE.
      * @param vbox the input vbox.
      * @param textArea the input textArea
-     * @throws IOException
      */
-    private void addItem(VBox vbox, TextArea textArea) throws IOException {
+    private void addItem(VBox vbox, TextArea textArea){
         HBox newStep = new HBox();
 
         //Set up Delete Button
         Button deleteButton = new Button("-");
         deleteButton.setStyle("-fx-text-fill: red");
-        deleteButton.setOnAction(event -> {
-            vbox.getChildren().remove(newStep);
-        });
+        deleteButton.setOnAction(event -> vbox.getChildren().remove(newStep));
         //Set user-visible list index and text area content
-        Label number = new Label("1) ");
+        Label number = new Label();
         Label step = new Label( textArea.getText() );
 
         //Add all new content to HBox then add HBox to parent VBox
         newStep.getChildren().addAll(deleteButton,number, step);
         vbox.getChildren().addAll(newStep);
+
+        //add step to recipe
+        recipe.addStep(textArea.getText());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        recipe = new Recipe();
     }
 }
--- src/main/java/Model/Recipe.java
+++ src/main/java/Model/Recipe.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
-    private List<String> steps = new ArrayList<>();
+    private final List<String> steps;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Recipe(String name) {
         this.name = name;
+        this.steps = new ArrayList<>();
+    }
+    public Recipe(){
+       this.steps = new ArrayList<>();
     }
 
     public void addStep(String step) {
         steps.add(step);
+    }
+
+    public int getSize(){
+        return steps.size();
     }
 }
