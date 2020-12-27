39cf8e80b3c02e9c00d12e2266b0ed3138075b53
Evan-Coulter <ercoulte@sfu.ca>
2020-12-26 18:42:03 -0800

    Add Clickable Buttons in View Recipe,
    added the buttons in view recipe's window for users to track which features they've finished.

--- src/main/java/Controllers/ViewController.java
+++ src/main/java/Controllers/ViewController.java
@@ -1,18 +1,22 @@
 package Controllers;
 
 import Controllers.UtilityControllers.GetRecipeController;
-import Controllers.UtilityControllers.PopUpBox;
 import Model.Recipe;
 import Model.RecipeManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ViewController implements Initializable, SavableController {
+    private Recipe recipe;
     @FXML
     private Label title;
     @FXML
@@ -20,11 +24,10 @@ public class ViewController implements Initializable, SavableController {
     @FXML
     private VBox steps;
 
-    private Recipe recipe;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         GetRecipeController getRecipeController = new GetRecipeController();
-        getRecipeController.drawWindow("Choose a Recipe", 400, 400);
+        getRecipeController.drawWindow("Choose a Recipe", 400, 100*RecipeManager.getInstance().size());
         recipe = getRecipeController.getRecipe();
         if(recipe == null){
             System.err.println("Error loading recipe!");
@@ -44,11 +47,46 @@ public class ViewController implements Initializable, SavableController {
         }
     }
 
-
     private void addRow(VBox parent, String rowText){
         Label label = new Label(rowText);
-        parent.getChildren().add(label);
+        Button toggleButton = new CustomToggleButton(label);
+        HBox row = getNewRow();
+        row.getChildren().addAll(toggleButton, label);
+        parent.getChildren().add(row);
+    }
+
+    private HBox getNewRow(){
+        HBox row = new HBox();
+        row.setAlignment(Pos.CENTER_LEFT);
+        row.setSpacing(8.0);
+        return row;
     }
 
     public void setUpClose() {}
+
+    public static class CustomToggleButton extends Button {
+        private boolean clicked;
+        private final Label associatedLabel;
+
+        public CustomToggleButton(Label associatedLabel){
+            this.setId("toggle-button");
+            this.clicked = false;
+            this.associatedLabel = associatedLabel;
+            setOnClick();
+        }
+
+        private void setOnClick() {
+            this.setOnAction(event->{
+                if(clicked){
+                    this.setStyle("-fx-background-color: white;");
+                    associatedLabel.setTextFill(Color.web("000000"));
+                    clicked = false;
+                }else{
+                    this.setStyle("-fx-background-color: #808080;");
+                    associatedLabel.setTextFill(Color.web("808080"));
+                    clicked = true;
+                }
+            });
+        }
+    }
 }
